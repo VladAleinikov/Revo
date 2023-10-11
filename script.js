7b3e7bb7ce6f0b5ad6f11fea0345fe753ea0919d
Vladik <vlad.a.aleynikov@gmail.com>
2023-10-11 14:25:34 +0300

    add coffe slider

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ video.addEventListener("click", e => {
       videoOverflow.classList.remove("hide");
       video.pause();
 })
+
+// Slider coffe
+const coffeSlider = [...document.getElementsByClassName("coffe__slides")];
+const sliderCard = [...document.getElementsByClassName("slider__card")];
+const coffeRight = document.getElementById("coffe__slide-right");
+let coffeSlide = 0;
+
+coffeRight.addEventListener("click", e => {
+      const show = document.documentElement.clientWidth > 768 ? 2 : 1;
+      coffeSlide += 1;
+      if (coffeSlide == (sliderCard.length / 2) - 1)
+            coffeSlide = 0;
+      coffeSlider[0].style.left = coffeSlide * -590 + "px";
+      coffeSlider[1].style.left = coffeSlide * -590 + "px";
+      sliderCard.map((card, id) => {
+            card.classList.remove("hide", "half-hide");
+            if (id == coffeSlide + show || id == coffeSlide + show + sliderCard.length / 2)
+                  card.classList.add("half-hide");
+            if (id > coffeSlide + show && id < sliderCard.length / 2 || id > coffeSlide + show + sliderCard.length / 2 && id < sliderCard.length)
+                  card.classList.add("hide");
+      })
+})
